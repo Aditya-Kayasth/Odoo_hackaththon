4c0db06471f04f3c70c4f6f5e454bd4f248a0756
Aditya-Kayasth <adikayasth@gmail.com>
2025-07-26 15:48:44 +0530

    Add Alert component for displaying success and error messages

---
+++ components/common/Altert.tsx
@@ -0,0 +1,30 @@
+import { IoIosAddCircleOutline, IoIosCheckboxOutline, IoIosInformationCircleOutline } from "react-icons/io";
+import { cn } from "@/lib/utils"
+import { BiError } from "react-icons/bi";
+
+const Altert = ({
+  success,
+  error,
+  message,
+}: {
+  success?: boolean;
+  error?: boolean;
+  message: string;
+}) => {
+  return <div className={cn("my-2 flex items-center gap-2 p-3 rounded-md",
+
+    success && "bg-green-100 text-green-500",
+    error && "bg-rose-100 text-rose-500"
+
+  )} >
+
+    <span>
+        {success && <IoIosCheckboxOutline size={20}/>}
+        {error && <BiError size={20}/>}
+
+    </span>
+    {message}
+  </div>;
+};
+
+export default Altert;
