1e9bc0bc826ebd3abac01ee367fff955053bab77
Aditya-Kayasth <adikayasth@gmail.com>
2025-07-28 09:52:46 +0530

    Add middleware for authentication and route protection

---
+++ middleware.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+
+import { authRoutes, publicRoutes, apiAuthPrefix , Loginredirect} from "./routes"
+ 
+const { auth: middleware } = NextAuth(authConfig)
+
+export default middleware((req) => {
+
+    const isLoggedIn = !!req.auth
+
+    console.log("IS LOGGED IN >>>>>", isLoggedIn)
+    const nextUrl = req.nextUrl
+
+    const isapiRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+    const isauthRoute = authRoutes.includes(nextUrl.pathname)
+
+    if (isapiRoute) {
+        return;
+    }
+    if (isauthRoute) {
+        if(isLoggedIn){
+            return Response.redirect(new URL(Loginredirect,nextUrl))
+        }
+        return;
+    }
+
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/login",nextUrl))
+    }
+
+    return;
+})
+
+export const config = {
+  matcher: [
+    // Exclude _next, static, favicon.ico, images, and all file extensions for static assets
+    '/((?!api|_next/static|_next/image|favicon.ico|static|images|.*\\.(?:css|js|json|png|jpg|jpeg|gif|svg|webp|ico|woff|woff2|ttf|eot|otf|txt|xml|csv|pdf|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
