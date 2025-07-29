65588f72b979ebcfbf358d65042c0f634b350584
Aditya-Kayasth <adikayasth@gmail.com>
2025-07-30 01:34:06 +0530

    Added event to the handlers to verify the users from the OAuth route

--- auth.ts
+++ auth.ts
@@ -2,9 +2,19 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import {db} from "@/lib/db"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { use } from "react"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   providers: authConfig.providers,
-})
+
+  events: {
+    async linkAccount({user}) {
+
+      await db.user.update({
+        where : { id:user.id },
+        data: {emailVerified: new Date()}
+      })
+    }
+}})
