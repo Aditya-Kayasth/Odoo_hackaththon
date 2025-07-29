642201a6e3d1534e38a2a707b35d7c475a574fe9
Aditya-Kayasth <adikayasth@gmail.com>
2025-07-29 17:52:46 +0530

    Creates login redirects and warnings

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ImBlogger } from "react-icons/im";
+
 import { loginSchema, LoginSchemaType } from "@/schemas/LoginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -7,6 +7,11 @@ import FormField from "@/components/common/FormField";
 import Button from "@/components/common/Button";
 import Heading from "@/components/common/Heading";
 import SocialAuth from "./SocialAuth";
+import { startTransition, useState, useTransition } from "react";
+import logIn from "@/actions/auth/login";
+import Altert from "../common/Altert";
+import { useRouter } from "next/navigation";
+import { Loginredirect } from "@/routes";
 
 const LoginForm = () => {
   const {
@@ -17,7 +22,31 @@ const LoginForm = () => {
     resolver: zodResolver(loginSchema),
   });
 
-  const onSubmit = (data: LoginSchemaType) => {};
+  const [error, seterror] = useState<string | undefined>("");
+  const [success, setsuccess] = useState<string | undefined>("");
+  const router = useRouter();
+  const [isLoading, setLoading] = useTransition();
+
+  const onSubmit = (data: LoginSchemaType) => {
+    startTransition(() => {
+      setsuccess("");
+      seterror("");
+      console.log("DATA >>>>>", data);
+      logIn(data).then((res) => {
+        if (res?.error) {
+          seterror(res?.error);
+          console.log("ERROR >>>>>", res?.error);
+        }
+        if (!res?.error) {
+          return router.push(Loginredirect);
+        }
+        // if (res?.success) {
+        //   setsuccess(res?.success);
+        //   console.log("SUCCESS >>>>>", res?.success, res.email, res.pass);
+        // }
+      });
+    });
+  };
 
   return (
     <form
@@ -38,11 +67,20 @@ const LoginForm = () => {
         register={register}
         errors={errors}
       />
-      <Button label="Login" type="submit" outline={true} />
+
+      {error && <Altert message={error} error />}
+      <Button
+        label={isLoading ? "Loading..." : "Login"}
+        disabled={isLoading}
+        type="submit"
+        outline={true}
+      />
 
       <div className="flex items-center my-2 w-full">
         <hr className="flex-grow border-t border-gray-300 dark:border-gray-800" />
-        <span className="mx-2 text-gray-500 text-sm dark:text-gray-700">OR</span>
+        <span className="mx-2 text-gray-500 text-sm dark:text-gray-700">
+          OR
+        </span>
         <hr className="flex-grow border-t border-gray-300 dark:border-gray-800" />
       </div>
       <div className="flex flex-col items-center w-full">
