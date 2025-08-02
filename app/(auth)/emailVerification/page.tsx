d829c8a725a6f2d1ca577b33764b58a4d63b50b2
Aditya-Kayasth <adikayasth@gmail.com>
2025-08-03 02:06:28 +0530

    Created Email verification functions for generating and sending tokens via email

---
+++ app/(auth)/emailVerification/page.tsx
@@ -0,0 +1,19 @@
+import EmailVerificationClient from "@/components/auth/EmailVerificationClient"
+import Container from "@/components/layout/Container"
+
+
+const page = () => {
+
+    
+  return (
+    
+      
+    <Container >
+
+        <EmailVerificationClient/>
+
+    </Container>
+  )
+}
+
+export default page
---
+++ lib/emailVerification.ts
@@ -0,0 +1,51 @@
+import { db } from "@/lib/db";
+import { v4 as uuidv4 } from "uuid";
+import { Resend } from "resend";
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationTocken = await db.emailVerification.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    return verificationTocken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const generateEmailVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 36000 * 1000);
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.emailVerification.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+  const newToken = await db.emailVerification.create({
+    data: { email, token, expiresAt: expires },
+  });
+  return newToken;
+};
+
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  const linkToVerify = `${process.env.BASE_URL}/emailVerification?token=${token}`;
+
+  const res = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Email Verification",
+    html: `<p>Click <a href = ${linkToVerify}>here</a> to verify </p>`,
+  });
+
+  return { error: res.error };
+};
